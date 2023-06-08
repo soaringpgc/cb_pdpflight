4a68c2b33c981cf58c6f2c55d442462f41fec20c
soaringpgc <soaringpgc@gmail.com>
2023-06-08 15:54:28 -0400

    Corrected one error in landing time . removed a bunch of commented out code.

--- inc/frontend/js/cb_pdp_flightlog.js
+++ inc/frontend/js/cb_pdp_flightlog.js
@@ -36,10 +36,6 @@
 //  		 	$('.Heading').css('display', 'none'); 	 	 	
 //  		 	$('.Row').css('display', 'none'); 	 	  
 	 });	 
-// 	 $('#reset_time_landing').on('click', function(e){
-// 	 console.log(this.localDivTag);
-// 	 	alert('reset landing');
-// 	 });
 	 
 	 
 	var app = app || {};
@@ -163,10 +159,8 @@
 		      var today = yyyy + '-' + mm + '-' + dd; 													
 			var launch =  new Date(today+'T'+(this.model.get('Takeoff')).replace(/-/g,"/"));
 			var temptime = Math.abs(landing.getTime()-launch.getTime())/3.6e6; 
-			var hours = Math.round(temptime *100) / 100 ;
-			
+			var hours = Math.round(temptime *100) / 100 ;			
 			this.$el.addClass('landed'); 
-// 			this.model.save();
 			this.model.save(
  				{ Landing:  landing.toLocaleTimeString('en-US',  {hour12:false}), Time: hours},
 				{
@@ -299,23 +293,21 @@
       	} else {
       		var updateModel = this.collection.get(formData.id);  
       	}
-//      	var old_takeoff = updateModel.get('Takeoff');
-//    		var old_landing = updateModel.get('Landing');  	
+     	var old_takeoff = updateModel.get('Takeoff');
+   		var old_landing = updateModel.get('Landing');  	
    		if(typeof formData['Takeoff'] === 'undefined' )  	{
    			formData['Takeoff'] = '00:00:00';	
    		}
   		if(typeof formData['Landing'] === 'undefined' )  	{
    			formData['Landing'] = '00:00:00';	
    		}
-//    		alert(formData['Landing'])  ;   
    		if ( (formData['Takeoff'] != '00:00:00' )  &&  ( formData['Landing'] != '00:00:00') ){
-//    			var old_takeoff = updateModel.get('Takeoff');
-//    			var old_landing = updateModel.get('Landing');
 			var now = new Date();
 			var dd = String(now.getDate()).padStart(2, '0');
 			var mm = String(now.getMonth() + 1).padStart(2, '0'); //January is 0!
 			var yyyy = now.getFullYear();			
 			var today = yyyy + '-' + mm + '-' + dd;
+			var launch =  new Date(today+'T'+(formData['Takeoff']).replace(/-/g,"/"));
 			var landing =  new Date(today+'T'+(formData['Landing']).replace(/-/g,"/"));
 			var temptime = Math.abs(landing.getTime()-launch.getTime())/3.6e6; 
 			var hours = Math.round(temptime *100) / 100 ;      	
@@ -323,7 +315,6 @@
    		} else {
    			formData['Time'] = '00:00:00';	   		
    		}
-     	            	       			
         updateModel.save(formData, {wait: true,
         	error: function(model, response, error){
       				var mresult= JSON.parse(response.responseText);     	
@@ -331,14 +322,10 @@
       		success: function(){
 //       				var mresult= JSON.parse(response.responseText);     	
       				}        	       
-        });
-         
+        });         
 // clean out the form:
 		this.cancelItem(e); 
 		$("#addorupdate").removeClass('editing');	
-// 		$('.Title').css('display', 'inline'); 	
-//  		$('.Heading').css('display', 'inline'); 	
-//   		$('.Row').css('display', 'table-row');  	 	
       	},
       cancelItem: function(e){
       	$(this.localDivTag).children('input').each(function(i, el ){
@@ -367,57 +354,6 @@
       		$('#Takeoff').val(null);
       	}
 	});
-// 	app.FlightsView = app.CollectionView.extend({
-// 	 	el: '#flights', 
-// 		localDivTag: '#addFlight Div',
-// 	 	preinitialize(){
-// 			this.collection = new app.FlightList();
-// 	 	},	
-// 	    initialize: function(){
-// 	      var self = this;
-// 	 	  this.collection = new app.FlightList();
-//     	  this.collection.fetch({reset:true, wait: true });    
-// 		  
-//           this.listenTo(this.collection, 'add', this.renderItem);
-//           this.listenTo(this.collection, 'reset', this.render);
-//         },
-//         render: function(){
-//       	  this.collection.each(function(item){
-//   		  this.renderItem(item);    	
-//       	}, this );
-//       },
-//       renderItem: function(item){    
-// 		// convert SQL time to Javascript   
-// 			if( item.get('Takeoff') != '00:00:00' ){
-// // 		 	if( (typeof item.get('Takeoff') !== 'undefined' ) && isNaN(item.get('Takeoff'))){
-// // 			if(item.get('Takeoff') != null  && item.get('Takeoff') != '00:00:00'){
-//       			var launch =  new Date((item.get('Takeoff')).replace(/-/g,"/"));
-// 				// adding 'inflight' class to the row. used below.... 
-//              	var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row inflight'});      			
-//       		} else {
-//        			item.set({'Takeoff': ""}, {silent: true });
-//       		}
-// 
-// //       		if( (item.get('Landing') !== '00:00:00' ) && isNaN(item.get('Landing'))){
-//  			if(  item.get('Landing') !=  '00:00:00' ){
-// //       		if( (typeof item.get('Landing') !== 'undefined' ) && isNaN(item.get('Landing'))){
-// //       		if(item.get('Landing') != null && item.get('Landing') != '00:00:00'){
-//       			var landing =  new Date(item.get('Landing'));
-//       			// add 'landed' class to this row... used below. 
-//       			var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row landed'});
-// //       			item.set({'end_display_time':  landing.toLocaleTimeString('en-US', { hour12:false})}, {silent: true });
-//       		} else {
-//       			item.set({'Landing': ""}, {silent: true });
-//       		}
-// 			if(expandedView === undefined ){
-//             	var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row'});
-//             }
-//             var itemView = new expandedView({
-//       	  		model: item
-//       		})
-//       		this.$el.append( itemView.render().el);   
-//         }
-// 	}); 
 	app.EditView = app.CollectionView.extend({
 	 	el: '#eflights', 
 		localDivTag: '#addFlight div',
@@ -439,20 +375,13 @@
       			var launch =  new Date((item.get('Takeoff')).replace(/-/g,"/"));
 				// adding 'inflight' class to the row. used below.... 
              	var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row inflight'});      			
-      		} // else {
-//        			item.set({'Takeoff': ""}, {silent: true });
-//       		}
-
- 			if(  item.get('Landing') !=  '00:00:00' ){
- 			
+      		} 
+ 			if(  item.get('Landing') !=  '00:00:00' ){ 			
 //  			if( (typeof item.get('Landing') !== 'undefined' ) && isNaN(item.get('Landing')) && (item.get('Landing') !=  '00:00:00') ){
       			var landing =  new Date(item.get('Landing'));
       			// add 'landed' class to this row... used below. 
-      			var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row landed'});
-      			
-      		} // else {
-//       			item.set({'Landing': ""}, {silent: true });
-//       		}
+      			var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row landed'});      			
+      		} 
 			if(expandedView === undefined ){
             	var expandedView = app.FlightView.extend({ localDivTag:this.localDivTag, className: 'Row'});
             }
